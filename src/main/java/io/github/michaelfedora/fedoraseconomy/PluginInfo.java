9698f21e30d627c1ba372e85aeac9fa5a715f0ac
Michael C <mtc120@gmail.com>
2016-08-19 17:13:26 -0400

    Updated versioning in PluginInfo

--- src/main/java/io/github/michaelfedora/fedoraseconomy/PluginInfo.java
+++ src/main/java/io/github/michaelfedora/fedoraseconomy/PluginInfo.java
@@ -6,7 +6,8 @@ package io.github.michaelfedora.fedoraseconomy;
 public abstract class PluginInfo {
     public static final String ID = "io.github.michaelfedora.fedoraseconomy";
     public static final String NAME = "FedorasEconomy";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.8.9-1.1.1";
+    //public static final String VERSION = "1.10.2-1.1.1";
     public static final String DESCRIPTION = "A Fedorian plugin that implements the Economy API.";
     public static final String AUTHORS = "Michael Fedora";
 
